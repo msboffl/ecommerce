7eb63f182946f074cdcf9197452aaf531c48bc90
Mareedu Saibabu <mareedusaibabu448@gmail.com>
2023-05-16 18:41:06 +0530

    auth Middleware

--- models/userModel.js
+++ models/userModel.js
@@ -2,36 +2,47 @@ import mongoose from "mongoose"; // Erase if already required
 import bcrypt from "bcrypt";
 
 // Declare the Schema of the Mongo model
-let userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    index: true,
+let userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: [{ type: mongoose.Schema.Types.ObjectId, ref: "Adderess" }],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   },
-  lastName: {
-    type: String,
-    required: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -8,12 +8,14 @@ import {
   updateSingleUser,
 } from "../controllers/userController.js";
 
+import { authMiddleware, isAdmin } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/allusers", getAllUsers);
-router.get("/:id", getSingleUser);
+router.get("/:id", authMiddleware, isAdmin, getSingleUser);
 router.delete("/:id", deleteSingleUser);
 router.put("/:id", updateSingleUser);
 
